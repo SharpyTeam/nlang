6aaa030e07a9848b753d6dca67c89239590ca819
Ilya Korobkov <serverprom@yandex.ru>
2020-01-12 21:55:42 +0300

    Fixed a typo

--- common/error_reporter.hpp
+++ common/error_reporter.hpp
@@ -14,7 +14,7 @@ public:
     virtual void ReportError(const std::string& error) = 0;
     virtual void ReportError(size_t row, size_t column, const std::string& error) = 0;
 
-    virtual ~ErrorReporter() = default;
+    virtual ~IErrorReporter() = default;
 };
 
 }
