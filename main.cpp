4ae9803231656324aa65514f2141e7a1dd0c86f9
Ilya Korobkov <serverprom@yandex.ru>
2020-03-20 13:45:23 +0300

    Removed debug code

--- main.cpp
+++ main.cpp
@@ -24,26 +24,6 @@ void print(const std::string &input) {
 int main(int argc, char *argv[]) {
     using namespace nlang;
 
-    Heap heap;
-
-    auto h = Int32::New(123);
-    auto h2 = Null::New();
-    auto h3 = String::New(heap, "123");
-    std::cout << std::to_string(h->IsBool()) << std::endl;
-    std::cout << std::to_string(h->IsInt32()) << std::endl;
-    std::cout << std::to_string(h->IsPointer()) << std::endl;
-    std::cout << std::to_string((uint8_t) h.GetType()) << std::endl;
-
-    std::cout << std::to_string(h2->IsBool()) << std::endl;
-    std::cout << std::to_string(h2->IsNull()) << std::endl << std::endl;
-
-    std::cout << h3.Is<Int32>() << std::endl;
-    std::cout << h3.Is<StackValue>() << std::endl;
-    std::cout << h3.Is<HeapValue>() << std::endl;
-    std::cout << h3.Is<String>() << std::endl;
-    std::cout << std::to_string((uint8_t)h3.GetType()) << std::endl;
-
-
     std::cout << "nlang " NLANG_VERSION " (" NLANG_BUILD_GIT_REVISION ", " NLANG_BUILD_PROCESSOR ",  " __DATE__ " " __TIME__  ")" << std::endl;
     std::cout << "[" << ((std::strcmp(NLANG_BUILD_COMPILER_ID, "GNU") == 0) ? "GCC" : NLANG_BUILD_COMPILER_ID) << " " NLANG_BUILD_COMPILER_VERSION << "]" << std::endl;
 
